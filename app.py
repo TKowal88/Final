85c1cca7ea25852e26bb9525e13c5a4c848aa7ba
Tomek Kowalczyk <tomekkowalczyk@tomeks-macbook-air.home>
2022-08-04 22:35:34 +0200

    Basic Structure

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
-from flask import Flask 
-from flask import render_template
+from flask import Flask, render_template, request
+from cs50 import SQL
 
 app = Flask(__name__)
 
-@app.route("/")
+db = SQL("sqlite:///warehouse.db")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("layout.html")
+    if request.method == "GET":
+        inventory = db.execute("SELECT * FROM stock ORDER BY code")
+        print(inventory)
+        boxlength = 50
+        storagewidth = 235
+        boxwidth = 35
+        storagelength = 590
+        widthpercentage = boxwidth // (storagelength / 100)
+        lengthpercentage = boxlength // (storagewidth / 100)
+        return render_template("index.html", inventory=inventory, lengthpercentage=lengthpercentage, widthpercentage=widthpercentage)
+    else:
+        code = request.form.get("code")
+        name = request.form.get("name")
+        db.execute("INSERT INTO stock (code, name) VALUES (?, ?)", code, name)
+        inventory = db.execute("SELECT * FROM stock ORDER BY code")
+        boxlength = 50
+        storagewidth = 235
+        storagelength = 590
+        boxwidth = 35
+        widthpercentage = boxwidth // (storagelength / 100)
+        lengthpercentage = boxlength // (storagewidth / 100)
+        return render_template("index.html", inventory=inventory, lengthpercentage=lengthpercentage, widthpercentage=widthpercentage)
+
+@app.route("/inventory")
+def inventory():
+    inventory = db.execute("SELECT * FROM stock ORDER BY code")
+    return render_template("inventory.html", inventory=inventory)
