bc4149086e20d314be94affc566c2eb1cdc3f85b
Tomek Kowalczyk <tomekkowalczyk@Tomeks-MacBook-Air.local>
2022-08-14 16:22:41 +0200

    drag and drop working, to add AJAX

--- static/script.js
+++ static/script.js
@@ -1,11 +1,30 @@
 
 $(document).ready(function(){
   $( function() {
-    $( ".dropzone" ).draggable({containment: ".storage"}, {snap: true});
+    var dragposition = '';
+    $( ".dropzone" ).draggable({containment: ".storage"},
     
-    
-  } );
+     {snap: true},
+     {stop: function(event, ui){
+      
+      dragposition = ui.position;
+      console.log(dragposition)
+    }   
 })
+  });
+
+let save = document.querySelector("#save")
+save.addEventListener("click", function() {
+  let items = document.querySelectorAll(".dropzone")
+for (let item of items) {
+let itemInfo = {top: item.offsetTop, left: item.offsetLeft, id: item.id};
+console.log(itemInfo)
+}
+})
+})
+
+
+
   
  
 
